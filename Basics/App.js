4d1c1f23856a6ed305400c131e425e1f8d8b5dcd
Bala_043 <balavignesh043@gmail.com>
2025-04-24 14:51:06 +0530

    Clean up files before starting fresh upload

--- Basics/App.js
+++
@@ -1,9 +0,0 @@
-const root=ReactDOM.createRoot(document.getElementById("root"))
-console.log(root)
-const child=React.createElement("h1",{id:"greeting",className:"greet"},"Hello World");
-console.log(child)
-//It Takes Three Argruments tag name, object which contains attributes of the elements and the third one is children
-root.render(child)
-
-
-
