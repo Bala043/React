f472977692f23db2873e099dae7937e70a45f6d5
Bala_043 <balavignesh043@gmail.com>
2025-04-24 15:10:26 +0530

    Removed all files from GitHub repo, kept locally

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const LOGO_URL="https://i.pinimg.com/originals/3d/a0/00/3da000e71ddc31ec29da41266b182ade.jpg"
-
-export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-
-export const MENU_URL="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.01020&lng=76.97010&restaurantId="
-
-export const LAND_PAGE="https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01020&lng=76.97010&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
--- src/utils/useSearch.js
+++
@@ -1,6 +0,0 @@
-import {useState} from 'react'
-const useSearch=()=>{
-    const [SearchText,setSearchText]=useState("")
-   return[SearchText,setSearchText]
-}
-export default useSearch
